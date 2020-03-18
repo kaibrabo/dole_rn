fbd58c76940e8656b5f10906b48d1d1b414c23f1
Kainoa Brabo <kaibrabo@gmail.com>
2020-03-17 17:21:10 -0700

    updated pay and transfer screens

--- App.js
+++ App.js
@@ -1,27 +1,29 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import ProfileScreen from "./components/ProfileScreen";
 import PayScreen from "./components/PayScreen";
 import PaymentsScreen from "./components/PaymentsScreen";
 import TransferScreen from "./components/TransferScreen";
-import TransactionsScreen from './components/TransactionsScreen';
+import TransactionsScreen from "./components/TransactionsScreen";
 
 const Stack = createStackNavigator();
 
 function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="cancel" component={ProfileScreen} />
-        <Stack.Screen name="Pay" component={PayScreen} />
-        <Stack.Screen name="Payments" component={PaymentsScreen} />
-        <Stack.Screen name="Transfer" component={TransferScreen} />
-        <Stack.Screen name="Transactions" component={TransactionsScreen} />
-
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={ProfileScreen} />
+                <Stack.Screen name="Pay" component={PayScreen} />
+                <Stack.Screen name="Payments" component={PaymentsScreen} />
+                <Stack.Screen name="Transfer" component={TransferScreen} />
+                <Stack.Screen
+                    name="Transactions"
+                    component={TransactionsScreen}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 export default App;
